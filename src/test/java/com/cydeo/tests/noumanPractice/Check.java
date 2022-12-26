e78e36d57e6b84b594ebd11dbfe0a3cb174a6077
Mohammad Nouman Babak <noumanbabak97@gmail.com>
2022-12-26 18:09:20 +0100

    Todays work 26DEc 12

---
+++ src/test/java/com/cydeo/tests/noumanPractice/Check.java
@@ -0,0 +1,8 @@
+package com.cydeo.tests.noumanPractice;
+
+public class Check {
+    public static int  sum(int a , int b){
+        int result = a+b;
+        return result;
+    }
+}
---
+++ src/test/java/com/cydeo/tests/noumanPractice/Sum.java
@@ -0,0 +1,7 @@
+package com.cydeo.tests.noumanPractice;
+
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("Check.sum(5,10) = " + Check.sum(5, 10));
+    }
+}
